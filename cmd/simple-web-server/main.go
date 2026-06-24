fa0c30e59f7beee5bd2cfac3a6845f20aa07e3c3
AFIF ZILANI <afifzilani4566@gmail.com>
2026-06-24 14:57:57 +0600

    feat: implement HTTP server with graceful shutdown and routing

--- cmd/simple-web-server/main.go
+++ cmd/simple-web-server/main.go
@@ -1,9 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/AFIF-ZILANI/simple-web-server/pkg/config"
+)
 
 func main() {
 
-	fmt.Println("Hello, World!")
+	cfg := config.MustLoadConfig()
 
+	router := http.NewServeMux()
+
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to students api"))
+		fmt.Fprintf(w, "Hello, World! This is a simple web server.")
+	})
+
+	server := http.Server{
+		Addr:    cfg.Address,
+		Handler: router,
+	}
+
+	fmt.Printf("Server is running on %s\n", cfg.Address)
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+
+		if err != nil {
+			fmt.Printf("Failed to start server: %s\n", err.Error())
+		}
+	}()
+
+	<-done
+
+	slog.Info("Shutting down the server...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Failed to gracefully shutdown the server", "error", err)
+	} else {
+		slog.Info("Server stopped gracefully")
+	}
+
+	slog.Info("Server shutdown successfully")
 }

a72ac3b483f67edbb7f183791b1b08e72a791228
AFIF ZILANI <afifzilani4566@gmail.com>
2026-06-25 02:17:40 +0600

    add: response serialization

--- pkg/http/handlers/student/student.go
+++ pkg/http/handlers/student/student.go
@@ -1,9 +1,26 @@
 package student
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+
+	"github.com/AFIF-ZILANI/simple-web-server/pkg/types"
+)
 
 func New() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// Implementation for creating a new student
+
+		var student types.Student
+
+		err := json.NewDecoder(r.Body).Decode(&student)
+
+		if errors.Is(err, io.EOF) {
+			http.Error(w, "Invalid JSON", http.StatusBadRequest)
+			return
+		}
+
+		w.Write([]byte("Create new Student"))
 	}
 }

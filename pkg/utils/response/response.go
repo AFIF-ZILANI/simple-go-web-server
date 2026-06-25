ad2db3ae4c46481ace23a9b14592576c76c9e742
AFIF ZILANI <afifzilani4566@gmail.com>
2026-06-26 03:43:32 +0600

    feat: enhance response structure with error handling and status constants

--- pkg/utils/response/response.go
+++ pkg/utils/response/response.go
@@ -5,10 +5,28 @@ import (
 	"net/http"
 )
 
+type Response struct {
+	Status string
+	Error  string
+}
+
+const (
+	StatusOK    = "OK"
+	StatusError = "ERROR"
+)
+
 func WriteJSON(w http.ResponseWriter, status int, data any) error {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 
 	return json.NewEncoder(w).Encode(data)
 
+}
+
+func GeneralError(err error) Response {
+
+	return Response{
+		Status: StatusError,
+		Error:  err.Error(),
+	}
 }
